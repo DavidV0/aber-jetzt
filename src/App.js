835aa195aa19d72abe6eec193b6db04bb1c3d35c
David Velickovic <davidveli2000@hotmail.com>
2023-04-11 19:28:50 +0200

    done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import "./App.css";
+import QuoteBox from "./components/QuoteBox";
+
+
+
+function App() {
+  return (
+    <div className="container">
+      <QuoteBox quote="" author="" />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/QuoteBox.js
@@ -0,0 +1,48 @@
+import "./QuoteBox.css"
+import React, { useState, useEffect } from 'react';
+
+
+
+const QuoteBox = ({quote, author}) => {
+const [quotes, setQuote] = useState('');
+const [authors, setAuthor] = useState('');
+
+useEffect(() => {
+    getQuote()
+  }, []);
+
+  const getQuote = () => {
+    let url = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    ;
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        let dataQuotes = data.quotes;
+        let randomNum = Math.floor(Math.random() * dataQuotes.length);
+        let randomQuote = dataQuotes[randomNum];
+
+        setQuote(randomQuote.quote);
+        setAuthor(randomQuote.author);
+      })
+  }
+ 
+
+   const handleClick = () => {
+    getQuote();
+   }
+    return (
+        <div id="quote-box">
+        <p id="text">{quote=quotes}</p>
+        <p id="author">{author=authors}</p>
+        <button id="new-quote" onClick={handleClick}>new Quote</button>
+        <a id="tweet-quote" href="twitter.com/intent/tweet">This will lead to the tweet</a>
+    </div>
+    )
+    
+
+    
+    
+};
+
+export default QuoteBox;
+
